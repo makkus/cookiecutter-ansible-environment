478de5b5d367023e78903cb342bb4d33b90a6038
Markus Binsteiner <makkus@gmail.com>
2017-01-06 19:28:43 +1300

    Add asking for sudo password if necessary.

--- {{cookiecutter.execution_dir}}/freckles_run.sh
+++ {{cookiecutter.execution_dir}}/freckles_run.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 cd {{cookiecutter.freckles_playbook_dir}}
 ../extensions/setup/role_update.sh
-ansible-playbook {{cookiecutter.freckles_install_playbook}}
+ansible-playbook {{cookiecutter.freckles_ask_sudo}} {{cookiecutter.freckles_install_playbook}}
 ansible-playbook {{cookiecutter.freckles_stow_playbook}}
