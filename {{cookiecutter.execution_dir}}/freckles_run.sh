e0ec889a492b774f37138c88b1fec05f9d659f7a
Markus Binsteiner <makkus@gmail.com>
2017-01-08 11:58:03 +1300

    Check whether its necessary to update roles.

--- {{cookiecutter.execution_dir}}/freckles_run.sh
+++ {{cookiecutter.execution_dir}}/freckles_run.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
+
+function update_roles {
+    ../extensions/setup/role_update.sh
+}
+
 cd {{cookiecutter.freckles_playbook_dir}}
-../extensions/setup/role_update.sh
+if [ ! -d ../roles/external/ansible-freckles ]; then
+    update_roles
+fi
+
 ansible-playbook {{cookiecutter.freckles_ask_sudo}} {{cookiecutter.freckles_install_playbook}}
 ansible-playbook {{cookiecutter.freckles_stow_playbook}}
