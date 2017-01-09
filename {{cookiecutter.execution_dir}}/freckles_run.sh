9eac404e9864481d1d5ed1b762dc2d2e3112ff9f
Markus Binsteiner <makkus@gmail.com>
2017-01-09 14:28:02 +1300

    Consolidated everything into one playbook.

--- {{cookiecutter.execution_dir}}/freckles_run.sh
+++ {{cookiecutter.execution_dir}}/freckles_run.sh
@@ -1,13 +1,5 @@
 #!/usr/bin/env bash
 
-function update_roles {
-    ../extensions/setup/role_update.sh
-}
-
 cd {{cookiecutter.freckles_playbook_dir}}
-if [ ! -d ../roles/external/ansible-freckles ]; then
-    update_roles
-fi
 
-ansible-playbook {{cookiecutter.freckles_ask_sudo}} {{cookiecutter.freckles_install_playbook}}
-ansible-playbook {{cookiecutter.freckles_stow_playbook}}
+ansible-playbook {{cookiecutter.freckles_ask_sudo}} {{cookiecutter.freckles_playbook}}
