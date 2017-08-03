0ef902c97ee87a2c2b65c98c4c2ddd9d41d32df5
Markus Binsteiner <makkus@posteo.de>
2017-08-03 10:25:19 +0700

    Added sourcing nix multi-user source script in case it exists

--- {{cookiecutter.env_dir}}/run_all_plays.sh
+++ {{cookiecutter.env_dir}}/run_all_plays.sh
@@ -2,9 +2,14 @@
 
 if [ -e "$HOME/.nix-profile/etc/profile.d/nix.sh" ]; then source "$HOME/.nix-profile/etc/profile.d/nix.sh"; fi
 
+if [ -e "/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh" ]; then source "/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh"; fi
+
+if [ -d "$HOME/.local/bin" ]; then
+    export PATH=$HOME/.local/bin:$PATH
+fi
+
 cd {{cookiecutter.playbook_dir}}
 
 {{cookiecutter.extra_script_commands}}
-
 
 ansible-playbook {{cookiecutter.ansible_playbook_args}} {{cookiecutter.ask_sudo}} {{cookiecutter.playbook}}
