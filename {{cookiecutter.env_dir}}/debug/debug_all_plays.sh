24115c0afdef5b530882e6f24ffcd2bf16ee8833
Markus Binsteiner <makkus@posteo.de>
2017-09-14 12:02:51 +0800

    added path(s) to debug run script

--- {{cookiecutter.env_dir}}/debug/debug_all_plays.sh
+++ {{cookiecutter.env_dir}}/debug/debug_all_plays.sh
@@ -2,6 +2,18 @@
 
 if [ -e "$HOME/.nix-profile/etc/profile.d/nix.sh" ]; then source "$HOME/.nix-profile/etc/profile.d/nix.sh"; fi
 
+if [ -d "$HOME/.local/bin" ]; then
+    export PATH="$HOME/.local/bin:$PATH"
+fi
+
+if [ -d "$HOME/.local/inaugurate/bin" ]; then
+    export PATH="$HOME/.local/inaugurate/bin:$PATH"
+fi
+
+if [ -d "$HOME/.local/inaugurate/conda/bin" ]; then
+    export PATH="$PATH:$HOME/.local/inaugurate/conda/bin"
+fi
+
 cd {{cookiecutter.playbook_dir}}/../debug
 
 {{cookiecutter.extra_script_commands}}
