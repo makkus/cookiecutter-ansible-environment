68d3da98df07de86b303b014bd8c0d0d5fbc5f15
Markus Binsteiner <makkus@posteo.de>
2018-04-05 14:39:57 +1200

    Adjusted inaugurate path to .local/share.

--- {{cookiecutter.env_dir}}/run_all_plays.sh
+++ {{cookiecutter.env_dir}}/run_all_plays.sh
@@ -4,18 +4,18 @@ if [ -e "$HOME/.nix-profile/etc/profile.d/nix.sh" ]; then source "$HOME/.nix-pro
 
 # if [ -e "/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh" ]; then source "/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh"; fi
 
-if [ -d "$HOME/.local/inaugurate/conda/envs/inaugurate/bin" ]; then
-    export PATH="$HOME/.local/inaugurate/conda/envs/inaugurate/bin:$PATH"
+if [ -d "$HOME/.local/share/inaugurate/conda/envs/inaugurate/bin" ]; then
+    export PATH="$HOME/.local/share//inaugurate/conda/envs/inaugurate/bin:$PATH"
 fi
-if [ -d "$HOME/.local/inaugurate/virtualenvs/inaugurate/bin" ]; then
-    export PATH="$HOME/.local/inaugurate/virtualenvs/inaugurate/bin:$PATH"
+if [ -d "$HOME/.local/share/inaugurate/virtualenvs/inaugurate/bin" ]; then
+    export PATH="$HOME/.local/share/inaugurate/virtualenvs/inaugurate/bin:$PATH"
 fi
-if [ -d "$HOME/.local/inaugurate/conda/bin" ]; then
-    export PATH="$HOME/.local/inaugurate/conda/bin:$PATH"
+if [ -d "$HOME/.local/share/inaugurate/conda/bin" ]; then
+    export PATH="$HOME/.local/share/inaugurate/conda/bin:$PATH"
 fi
 
-if [ -d "$HOME/.local/inaugurate/bin" ]; then
-    export PATH="$HOME/.local/inaugurate/bin:$PATH"
+if [ -d "$HOME/.local/share/inaugurate/bin" ]; then
+    export PATH="$HOME/.local/share/inaugurate/bin:$PATH"
 fi
 
 if [ -d "$HOME/.local/bin" ]; then
